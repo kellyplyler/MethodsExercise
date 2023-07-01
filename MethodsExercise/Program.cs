1a127f36b8a2f67bdc2993f74958db3f61412996
Kelly Plyler <kellysplyler@gmail.com>
2023-07-01 08:54:49 -0500

    finished Methods exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+            Console.WriteLine("Are you a man or a woman?");
+                string gender = Console.ReadLine();
+            Console.WriteLine("Where do you live?");
+                string location = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+                string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+                var animal = Console.ReadLine();
+            Console.WriteLine($"{name} is a {gender} who lives in {location} with their pet {animal} in a {color} house.");
+            
         }
     }
 }
